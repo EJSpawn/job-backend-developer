cfbae13abb5ed54c3542abc44446611d97970e10
Elvis Justino da Silva <elvis.justino@hotmail.com>
2019-07-29 10:08:25 -0300

    Primeiro commit

---
+++ src/test/java/com/project/jobbackenddeveloper/JobBackendDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jobbackenddeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBackendDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
