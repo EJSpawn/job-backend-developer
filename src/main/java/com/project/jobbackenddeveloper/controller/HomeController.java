bf41f6d3c4d87c58ca3327364ee732dbeca17fe4
Elvis Justino da Silva <elvis.justino@hotmail.com>
2019-07-30 05:33:48 -0300

    Correçao de Build para o Heroku

--- src/main/java/com/project/jobbackenddeveloper/controller/HomeController.java
+++ src/main/java/com/project/jobbackenddeveloper/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.project.jobbackenddeveloper.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +20,9 @@ public class HomeController {
 		this.newsService = newsService;
 	}
 	
-	@GetMapping()
+	@GetMapping(produces = MediaType.TEXT_HTML_VALUE)
 	public String index(Model model) {	
 		model.addAttribute("newsList", newsService.findAll());
-		return "/home.html";
+		return "home.html";
 	}
 }
